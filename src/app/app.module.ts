2c07be28e9a79c01d595e3c7075604103bc406e1
bderevyaga <bogdan.derevyaga@gmail.com>
2018-06-30 23:11:31 +0300

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,35 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
+import {
+    MatButtonModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule
+} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import {ElectronService} from './providers/electron.service';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatListModule,
+        MatIconModule,
+        MatCardModule,
+        MatGridListModule,
+        MatInputModule
     ],
     providers: [ElectronService],
     bootstrap: [AppComponent]
